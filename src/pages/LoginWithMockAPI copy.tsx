265166c5cb7d997bf31a874e7188ecda2ced2b43
sonhyangji <ths3437@naver.com>
2023-10-06 18:51:24 +0900

    [edit] 1w

--- src/pages/LoginWithMockAPI copy.tsx
+++ src/pages/LoginWithMockAPI copy.tsx
@@ -1,4 +1,4 @@
-import React, { useState }  from 'react'
+import React, { useState } from 'react'
 
 type LoginSuccessMessage = 'SUCCESS'
 type LoginFailMessage = 'FAIL'
@@ -7,7 +7,7 @@ interface LoginResponse {
   message: LoginSuccessMessage | LoginFailMessage
   token: string
 }
-/** ----------------------1회차-------------------------- */
+
 interface UserInfo {
   name: string
 }
@@ -37,10 +37,8 @@ const users: User[] = [
 ]
 
 const _secret: string = '1234qwer!@#$'
-/** ----------------------1회차-------------------------- */
 
 const login = async (username: string, password: string): Promise<LoginResponse | null> => {
-  // TODO: 올바른 username, password를 입력하면 {message: 'SUCCESS', token: (원하는 문자열)} 를 반환하세요.
   const user: User | undefined = users.find((user: User) => {
     return user.username === username && user.password === password
   })
@@ -49,9 +47,7 @@ const login = async (username: string, password: string): Promise<LoginResponse 
     : null
 }
 
-//const getUserInfo = async (): Promise<{ username: string } | null> =>
 const getUserInfo = async (token: string): Promise<UserInfo | null> => {
-  // TODO: login 함수에서 받은 token을 이용해 사용자 정보를 받아오세요.
   const parsedToken = JSON.parse(token)
   if (!parsedToken?.secret || parsedToken.secret !== _secret) return null
 
@@ -62,15 +58,13 @@ const getUserInfo = async (token: string): Promise<UserInfo | null> => {
 }
 
 const LoginWithMockAPI = () => {
-  // 1회차 추가부분
   const [userInfo, setUserInfo] = useState<UserInfo>({name: ''})
 
   const loginSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    // TODO: form 에서 username과 password를 받아 login 함수를 호출하세요.
-    const formData = new FormData(event.currentTarget);
-    //for (let [key, value] of formData.entries()) console.log(key, value);
+    const formData = new FormData(event.currentTarget)
+
     const loginRes = await login(formData.get('username') as string, formData.get('password') as string)
     if(!loginRes) return
 
@@ -85,7 +79,6 @@ const LoginWithMockAPI = () => {
       Login with Mock API
     </h1>
     <form onSubmit={loginSubmitHandler}>
-      {/* TODO: 여기에 username과 password를 입력하는 input을 추가하세요. 제출을 위해 button도 추가하세요. */}
       <label>
         Username:
         <input type="text" name="username"/>
@@ -100,9 +93,7 @@ const LoginWithMockAPI = () => {
       <h2>
         User info
       </h2>
-      {/* TODO: 유저 정보를 보여주도록 구현하세요. 필요에 따라 state나 다른 변수를 추가하세요. */}
       {JSON.stringify(userInfo)}
-      {/*JSON.stringify({username: 'blueStragglr'})*/}
     </div>
   </div>)
 }
